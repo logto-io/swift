eda8019c5cf16a6332802446ed787a14b499ca56
Gao Sun <gao@silverhand.io>
2022-01-07 15:57:08 +0800

    feat: `IDTokenClaims` type

---
+++ Sources/Logto/Types/IdTokenClaims.swift
@@ -0,0 +1,17 @@
+//
+//  IdTokenClaims.swift
+//  
+//
+//  Created by Gao Sun on 2022/1/7.
+//
+
+import Foundation
+
+public struct IDTokenClaims {
+    let sub: String
+    let atHash: String
+    let aud: String
+    let exp: Int64
+    let iat: Int64
+    let iss: String
+}
