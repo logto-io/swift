381e7016b76e9628fbbc210cdea270c2ca0d8dd2
Gao Sun <gao@silverhand.io>
2022-01-12 17:51:15 +0800

    feat: `decodeIdToken()` (#8)
    * feat: `decodeIdToken()`
    
    * chore: fix format
    
    * chore: fix format
    
    * fix: typos

--- Sources/Logto/Extensions/String.swift
+++ Sources/Logto/Extensions/String.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 extension String {
+    static func fromUrlSafeBase64(string: String) -> String? {
+        guard let data = Data.fromUrlSafeBase64(string: string) else {
+            return nil
+        }
+        return String(data: data, encoding: .utf8)
+    }
+
     var isUrlSafe: Bool {
         !contains(where: { "+/=".contains($0) })
     }
