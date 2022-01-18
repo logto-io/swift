6d3fa37afb04d00f7d320ee4f1df9b96f2bfa22e
Gao Sun <gao@silverhand.io>
2022-01-18 11:00:40 +0800

    refactor: extract `verifyJws()`

--- Sources/Logto/LogtoUtilities+IdToken.swift
+++ Sources/Logto/LogtoUtilities+IdToken.swift
@@ -50,39 +50,7 @@ extension LogtoUtilities {
 
         // Public key verification
         let jws = try JWS(compactSerialization: idToken)
-
-        guard let algorithm = jws.header.algorithm else {
-            throw LogtoErrors.Verification.jwtMissingAlgorithmInHeader
-        }
-
-        let verifiers: [Verifier] = try jwks
-            .keys
-            .compactMap {
-                // [RFC-7518](https://tools.ietf.org/html/rfc7518#section-7.4)
-                switch $0 {
-                case let publicKey as ECPublicKey:
-                    if let secKey = try? publicKey.converted(to: SecKey.self) {
-                        return Verifier(verifyingAlgorithm: algorithm, key: secKey)
-                    }
-                case let publicKey as RSAPublicKey:
-                    if let secKey = try? publicKey.converted(to: SecKey.self) {
-                        return Verifier(verifyingAlgorithm: algorithm, key: secKey)
-                    }
-                case let symmetricKey as SymmetricKey:
-                    if let data = try? symmetricKey.converted(to: Data.self) {
-                        return Verifier(verifyingAlgorithm: algorithm, key: data)
-                    }
-                default:
-                    throw LogtoErrors.Verification.unsupportedJwkType
-                }
-                return nil
-            }
-
-        guard verifiers.contains(where: {
-            (try? jws.validate(using: $0)) != nil ? true : false
-        }) else {
-            throw LogtoErrors.Verification.noSigningKeyMatched
-        }
+        try verifyJws(jws, jwks: jwks)
 
         // Claims verification
         let claims = try decodeIdToken(idToken)
