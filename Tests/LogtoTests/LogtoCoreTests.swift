13e2ec8eeff62117e48c75cbab79135889de5eb5
Gao Sun <gao@silverhand.io>
2022-01-18 18:37:56 +0800

    feat: `verifyAndParseSignInCallbackUri()`

--- Tests/LogtoTests/LogtoCoreTests.swift
+++ Tests/LogtoTests/LogtoCoreTests.swift
@@ -105,4 +105,39 @@ final class LogtoCoreTests: XCTestCase {
             "client_id=foo&redirect_uri=logto://sign-in/redirect&code_challenge=\(codeChallenge)&code_challenge_method=S256&state=\(state)&response_type=authorization_code&prompt=consent&resource=https://api.logto.dev/&resource=bar"
         )
     }
+
+    func testVerifyAndParseSignInCallbackUri() throws {
+        let state = "123"
+        let code = "456"
+        let callbackUri = "logto-dev://callback?state=\(state)&code=\(code)"
+        let redirectUri = "logto-dev://callback"
+
+        XCTAssertThrowsError(try LogtoCore
+            .verifyAndParseSignInCallbackUri("aaa", redirectUri: redirectUri, state: state)) {
+                XCTAssertEqual($0 as? LogtoErrors.UriVerification, LogtoErrors.UriVerification.redirectUriMismatched)
+            }
+
+        XCTAssertThrowsError(try LogtoCore
+            .verifyAndParseSignInCallbackUri(redirectUri, redirectUri: redirectUri, state: state)) {
+                XCTAssertEqual($0 as? LogtoErrors.UriVerification, LogtoErrors.UriVerification.stateMismatched)
+            }
+
+        XCTAssertThrowsError(try LogtoCore
+            .verifyAndParseSignInCallbackUri(callbackUri + "&error=foo", redirectUri: redirectUri, state: state)) {
+                XCTAssertEqual(
+                    $0 as? LogtoErrors.UriVerification,
+                    LogtoErrors.UriVerification.errorItemFound(items: [URLQueryItem(name: "error", value: "foo")])
+                )
+            }
+
+        XCTAssertThrowsError(try LogtoCore
+            .verifyAndParseSignInCallbackUri(redirectUri + "?state=123", redirectUri: redirectUri, state: state)) {
+                XCTAssertEqual($0 as? LogtoErrors.UriVerification, LogtoErrors.UriVerification.missingCode)
+            }
+
+        XCTAssertEqual(
+            try LogtoCore.verifyAndParseSignInCallbackUri(callbackUri, redirectUri: redirectUri, state: state),
+            "456"
+        )
+    }
 }
