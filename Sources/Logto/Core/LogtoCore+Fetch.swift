dd5b60803709abc2436f0185f6076e6f9d28892b
Gao Sun <gao@silverhand.io>
2022-01-19 13:35:42 +0800

    feat: `fetchOidcConfig()` (#17)
    * feat: `fetchOidcConfig()`
    
    * refactor: callback -> completion
    
    * fix: format
    
    * test: add unit test

---
+++ Sources/Logto/Core/LogtoCore+Fetch.swift
@@ -0,0 +1,23 @@
+//
+//  LogtoCore+Fetch.swift
+//
+//
+//  Created by Gao Sun on 2022/1/18.
+//
+
+import Foundation
+
+extension LogtoCore {
+    struct OidcConfigResponse: Codable, Equatable {
+        let authorizationEndpoint: String
+        let tokenEndpoint: String
+        let endSessionEndpoint: String
+        let revocationEndpoint: String
+        let jwksUri: String
+        let issuer: String
+    }
+
+    static func fetchOidcConfig(endpoint: String, completion: @escaping (OidcConfigResponse?, Error?) -> Void) {
+        Utilities.httpGet(endpoint: endpoint, completion: completion)
+    }
+}
---
+++ Sources/Logto/Utilities/Utilities.swift
@@ -0,0 +1,47 @@
+//
+//  Utilities.swift
+//
+//  Internal utilities.
+//
+//  Created by Gao Sun on 2022/1/18.
+//
+
+import Foundation
+
+enum Utilities {
+    static func getCamelCaseDecoder() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
+
+    static func httpGet<T: Codable>(endpoint: String, completion: @escaping (T?, Error?) -> Void) {
+        guard let url = URL(string: endpoint) else {
+            completion(nil, LogtoErrors.UrlConstruction.unableToConstructUrl)
+            return
+        }
+
+        let decoder = Utilities.getCamelCaseDecoder()
+
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard error == nil else {
+                completion(nil, error)
+                return
+            }
+
+            guard let data = data else {
+                completion(nil, LogtoErrors.Request.noResponseData)
+                return
+            }
+
+            do {
+                let decoded = try decoder.decode(T.self, from: data)
+                completion(decoded, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+
+        task.resume()
+    }
+}
