5307294fc46d9b8f98eb99568920aecb169005dd
Gao Sun <gao@silverhand.io>
2022-01-24 16:34:11 +0800

    refactor: LogtoRequest (#30)

---
+++ Sources/Logto/Utilities/LogtoRequest.swift
@@ -0,0 +1,113 @@
+//
+//  LogtoRequest.swift
+//
+//
+//  Created by Gao Sun on 2022/1/24.
+//
+
+import Foundation
+
+enum LogtoRequest {
+    enum HttpMethod: String {
+        case get = "GET"
+        case post = "POST"
+    }
+
+    private static func handleResponse<T: Codable>(
+        data: Data?,
+        error: Error?,
+        completion: @escaping HttpCompletion<T>
+    ) {
+        let decoder = LogtoUtilities.getCamelCaseDecoder()
+
+        guard error == nil else {
+            completion(nil, error)
+            return
+        }
+
+        guard let data = data else {
+            completion(nil, LogtoErrors.Request.noResponseData)
+            return
+        }
+
+        do {
+            let decoded = try decoder.decode(T.self, from: data)
+            completion(decoded, nil)
+        } catch {
+            completion(nil, error)
+        }
+    }
+
+    static func load(
+        useSession session: NetworkSession,
+        method: HttpMethod,
+        url: URL,
+        headers: [String: String] = [:],
+        body: Data? = nil,
+        completion: @escaping HttpCompletion<Data>
+    ) {
+        var request = URLRequest(
+            url: url,
+            cachePolicy: .reloadIgnoringLocalCacheData
+        )
+
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        headers.forEach { key, value in
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+
+        session.loadData(with: request, completion: completion)
+    }
+
+    static func load(
+        useSession session: NetworkSession,
+        method: HttpMethod,
+        endpoint: String,
+        headers: [String: String] = [:],
+        body: Data? = nil,
+        completion: @escaping HttpCompletion<Data>
+    ) {
+        guard let url = URL(string: endpoint) else {
+            completion(nil, LogtoErrors.UrlConstruction.unableToConstructUrl)
+            return
+        }
+
+        load(useSession: session, method: method, url: url, headers: headers, body: body, completion: completion)
+    }
+
+    static func get<T: Codable>(
+        useSession session: NetworkSession,
+        endpoint: String,
+        headers: [String: String] = [:],
+        completion: @escaping HttpCompletion<T>
+    ) {
+        load(useSession: session, method: .get, endpoint: endpoint, headers: headers) { data, error in
+            handleResponse(data: data, error: error, completion: completion)
+        }
+    }
+
+    static func post<T: Codable>(
+        useSession session: NetworkSession,
+        endpoint: String,
+        headers: [String: String] = [:],
+        body: Data? = nil,
+        completion: @escaping HttpCompletion<T>
+    ) {
+        load(useSession: session, method: .post, endpoint: endpoint, headers: headers, body: body) { data, error in
+            handleResponse(data: data, error: error, completion: completion)
+        }
+    }
+
+    static func post(
+        useSession session: NetworkSession,
+        endpoint: String,
+        headers: [String: String] = [:],
+        body: Data? = nil,
+        completion: @escaping HttpEmptyCompletion
+    ) {
+        load(useSession: session, method: .post, endpoint: endpoint, headers: headers, body: body) {
+            completion($1)
+        }
+    }
+}
--- Tests/LogtoTests/URLSessionMock.swift
+++ Tests/LogtoTests/URLSessionMock.swift
@@ -14,60 +14,55 @@ class NetworkSessionMock: NetworkSession {
     static let shared = NetworkSessionMock()
 
     func loadData(
-        with url: URL,
-        completion: @escaping HttpCompletion<Data>
-    ) {
-        switch url.path {
-        case "/oidc_config:good":
-            completion(Data("""
-                {
-                    "authorization_endpoint": "https://logto.dev/oidc/auth",
-                    "token_endpoint": "https://logto.dev/oidc/token",
-                    "end_session_endpoint": "https://logto.dev/oidc/session/end",
-                    "revocation_endpoint": "https://logto.dev/oidc/token/revocation",
-                    "jwks_uri": "https://logto.dev/oidc/jwks",
-                    "issuer": "http://localhost:443/oidc"
-                }
-            """.utf8), nil)
-        default:
-            completion(nil, MockError())
-        }
-    }
-
-    func loadData(
         with request: URLRequest,
         completion: @escaping HttpCompletion<Data>
     ) {
-        guard request.httpMethod != nil else {
-            completion(nil, MockError())
-            return
-        }
-
-        switch request.url?.path {
-        case "/token:good":
-            completion(Data("""
-                {
-                    "access_token": "123",
-                    "refresh_token": "456",
-                    "id_token": "789",
-                    "token_type": "jwt",
-                    "scope": "",
-                    "expires_in": 123
+        switch request.httpMethod {
+        case "GET":
+            switch request.url?.path {
+            case "/oidc_config:good":
+                completion(Data("""
+                    {
+                        "authorization_endpoint": "https://logto.dev/oidc/auth",
+                        "token_endpoint": "https://logto.dev/oidc/token",
+                        "end_session_endpoint": "https://logto.dev/oidc/session/end",
+                        "revocation_endpoint": "https://logto.dev/oidc/token/revocation",
+                        "jwks_uri": "https://logto.dev/oidc/jwks",
+                        "issuer": "http://localhost:443/oidc"
+                    }
+                """.utf8), nil)
+            case "/user":
+                guard request.value(forHTTPHeaderField: "Authorization") == "Bearer good" else {
+                    completion(nil, MockError())
+                    return
                 }
-            """.utf8), nil)
-        case "/user":
-            guard request.value(forHTTPHeaderField: "Authorization") == "Bearer good" else {
+
+                completion(Data("""
+                    {
+                        "sub": "foo"
+                    }
+                """.utf8), nil)
+            default:
                 completion(nil, MockError())
-                return
             }
-
-            completion(Data("""
-                {
-                    "sub": "foo"
-                }
-            """.utf8), nil)
-        case "/revoke:good":
-            completion(nil, nil)
+        case "POST":
+            switch request.url?.path {
+            case "/token:good":
+                completion(Data("""
+                    {
+                        "access_token": "123",
+                        "refresh_token": "456",
+                        "id_token": "789",
+                        "token_type": "jwt",
+                        "scope": "",
+                        "expires_in": 123
+                    }
+                """.utf8), nil)
+            case "/revoke:good":
+                completion(nil, nil)
+            default:
+                completion(nil, MockError())
+            }
         default:
             completion(nil, MockError())
         }
