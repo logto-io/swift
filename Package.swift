4ecacf896071915affe04fe54206d0400b31d24e
Gao Sun <gao@silverhand.io>
2022-01-27 17:25:43 +0800

    fix: build

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LogtoMock",
-            dependencies: [],
+            dependencies: ["Logto"],
             path: "Tests/LogtoMock"
         ),
         .target(
