93722ddfddaaf1ce0b35ad111bbfd8b3cfe669f7
Gao Sun <gao@silverhand.io>
2022-03-30 14:57:52 +0800

    feat(client): web social sign in (#64)

--- Sources/LogtoClient/LogtoAuthSession/LogtoWebViewAuthViewController.swift
+++ Sources/LogtoClient/LogtoAuthSession/LogtoWebViewAuthViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Gao Sun on 2022/3/22.
 //
 
+import AuthenticationServices
 import Foundation
 import WebKit
 
@@ -25,9 +26,80 @@ public class LogtoWebViewAuthViewController: UnifiedViewController {
     override public func loadView() {
         view = webView
         webView.navigationDelegate = authSession
+        webView.configuration.userContentController.add(self, name: "socialHandler")
     }
 
     override public func viewDidLoad() {
         webView.load(URLRequest(url: authSession.uri))
+    }
+
+    func postErrorMessage(_ error: LogtoWebViewAuthViewError) {
+        webView.evaluateJavaScript("""
+            window.postMessage({
+                type: 'error',
+                code: '\(error.code)',
+                description: '\(error.localizedDescription)'
+            }, location.origin);
+        """)
+    }
+}
+
+extension LogtoWebViewAuthViewController: ASWebAuthenticationPresentationContextProviding {
+    public func presentationAnchor(for _: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        view.window!
+    }
+}
+
+extension LogtoWebViewAuthViewController: WKScriptMessageHandler {
+    static let webAuthCallbackScheme = "logto-callback"
+
+    struct SocialPostBody: Codable {
+        static func safeParseJson(json: Any) -> SocialPostBody? {
+            do {
+                let data = try JSONSerialization.data(withJSONObject: json)
+                return try? JSONDecoder().decode(self, from: data)
+            } catch {
+                return nil
+            }
+        }
+
+        let callbackUri: String
+        let redirectTo: String
+    }
+
+    public func userContentController(_: WKUserContentController, didReceive message: WKScriptMessage) {
+        guard let body = SocialPostBody.safeParseJson(json: message.body),
+              let redirectUri = URL(string: body.redirectTo),
+              let callbackUri = URL(string: body.callbackUri),
+              var callbackComponents = URLComponents(url: callbackUri, resolvingAgainstBaseURL: true)
+        else {
+            return
+        }
+
+        let session = ASWebAuthenticationSession(
+            url: redirectUri,
+            callbackURLScheme: LogtoWebViewAuthViewController.webAuthCallbackScheme
+        ) { [self] customUri, error in
+            guard let customUri = customUri else {
+                postErrorMessage(.webAuthFailed(innerError: error))
+                return
+            }
+
+            // Construct callback URL for WebView
+            let customComponents = URLComponents(url: customUri, resolvingAgainstBaseURL: true)
+            callbackComponents
+                .queryItems = (callbackComponents.queryItems ?? []) + (customComponents?.queryItems ?? [])
+
+            guard let url = callbackComponents.url else {
+                postErrorMessage(.unableToConstructCallbackUri)
+                return
+            }
+
+            self.webView.load(URLRequest(url: url))
+        }
+
+        session.prefersEphemeralWebBrowserSession = true
+        session.presentationContextProvider = self
+        session.start()
     }
 }
