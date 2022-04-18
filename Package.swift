3bcae404b9dead09f9aaf901dbcf6eb4bb65b2fd
Gao Sun <gao@silverhand.io>
2022-04-18 17:47:26 +0800

    fix: SwiftPM build (#86)
    * fix: make LogtoMock a test target
    
    * fix: check OS for Alipay and WeChat plugin
    
    * refactor: rename package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Logto SDK",
+    name: "LogtoSDK",
     platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v5)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -43,13 +43,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
+            name: "Logto",
+            dependencies: ["JOSESwift"]
+        ),
+        .target(
             name: "LogtoMock",
             dependencies: ["Logto"],
             path: "Tests/LogtoMock"
-        ),
-        .target(
-            name: "Logto",
-            dependencies: ["JOSESwift"]
         ),
         .testTarget(
             name: "LogtoTests",
--- Sources/LogtoSocialPluginAlipay/LogtoSocialPluginAlipay.swift
+++ Sources/LogtoSocialPluginAlipay/LogtoSocialPluginAlipay.swift
@@ -5,94 +5,99 @@
 //  Created by Gao Sun on 2022/3/31.
 //
 
-@_exported import AFServiceSDK
-import Foundation
-import LogtoSocialPlugin
+#if !os(macOS)
 
-public enum LogtoSocialPluginAlipayError: LogtoSocialPluginError {
-    case authFailed(withCode: String)
+    @_exported import AFServiceSDK
+    import Foundation
+    import LogtoSocialPlugin
 
-    public var code: String {
-        switch self {
-        case .authFailed:
-            return "auth_failed"
+    public enum LogtoSocialPluginAlipayError: LogtoSocialPluginError {
+        case authFailed(withCode: String)
+
+        public var code: String {
+            switch self {
+            case .authFailed:
+                return "auth_failed"
+            }
+        }
+
+        var localizedDescription: String {
+            switch self {
+            case let .authFailed(withCode):
+                return "Alipay auth failed with code \(withCode)."
+            }
         }
     }
 
-    var localizedDescription: String {
-        switch self {
-        case let .authFailed(withCode):
-            return "Alipay auth failed with code \(withCode)."
-        }
-    }
-}
+    // Follows Alipay official docs: https://opendocs.alipay.com/open/218/wy75xo
+    public class LogtoSocialPluginAlipay: LogtoSocialPlugin {
+        public let connectorId = "alipay"
+        public let urlSchemes = ["alipay"]
 
-// Follows Alipay official docs: https://opendocs.alipay.com/open/218/wy75xo
-public class LogtoSocialPluginAlipay: LogtoSocialPlugin {
-    public let connectorId = "alipay"
-    public let urlSchemes = ["alipay"]
+        public init() {}
 
-    public init() {}
-
-    public func start(_ configuration: LogtoSocialPluginConfiguration) {
-        guard let redirectComponents = URLComponents(url: configuration.redirectUri, resolvingAgainstBaseURL: true)
-        else {
-            configuration.errorHandler(LogtoSocialPluginUriError.unableToConstructRedirectComponents)
-            return
-        }
-
-        guard var callbackComponents = URLComponents(url: configuration.callbackUri, resolvingAgainstBaseURL: true)
-        else {
-            configuration.errorHandler(LogtoSocialPluginUriError.unableToConstructCallbackComponents)
-            return
-        }
-
-        var components = URLComponents()
-
-        components.scheme = "https"
-        components.host = "authweb.alipay.com"
-        components.path = "/auth"
-        components.queryItems = [
-            URLQueryItem(name: "auth_type", value: "PURE_OAUTH_SDK"),
-            URLQueryItem(name: "scope", value: "auth_user"),
-        ] + (redirectComponents.queryItems ?? [])
-
-        AFServiceCenter.call(.auth, withParams: [
-            kAFServiceOptionBizParams: [
-                "url": components.string,
-            ],
-            kAFServiceOptionCallbackScheme: "logto-demo",
-        ]) { response in
-            guard let response = response, response.responseCode == AFResCode.success else {
-                configuration
-                    .errorHandler(LogtoSocialPluginAlipayError
-                        .authFailed(withCode: response?.responseCode.rawValue.description ?? "unknown"))
+        public func start(_ configuration: LogtoSocialPluginConfiguration) {
+            guard let redirectComponents = URLComponents(url: configuration.redirectUri, resolvingAgainstBaseURL: true)
+            else {
+                configuration.errorHandler(LogtoSocialPluginUriError.unableToConstructRedirectComponents)
                 return
             }
 
-            callbackComponents.queryItems = (callbackComponents.queryItems ?? []) + response.result.map { key, value in
-                URLQueryItem(name: key.description, value: String(describing: value))
-            }
-
-            guard let url = callbackComponents.url else {
-                configuration.errorHandler(LogtoSocialPluginUriError.unableToConstructCallbackUri)
+            guard var callbackComponents = URLComponents(url: configuration.callbackUri, resolvingAgainstBaseURL: true)
+            else {
+                configuration.errorHandler(LogtoSocialPluginUriError.unableToConstructCallbackComponents)
                 return
             }
 
-            configuration.completion(url)
+            var components = URLComponents()
+
+            components.scheme = "https"
+            components.host = "authweb.alipay.com"
+            components.path = "/auth"
+            components.queryItems = [
+                URLQueryItem(name: "auth_type", value: "PURE_OAUTH_SDK"),
+                URLQueryItem(name: "scope", value: "auth_user"),
+            ] + (redirectComponents.queryItems ?? [])
+
+            AFServiceCenter.call(.auth, withParams: [
+                kAFServiceOptionBizParams: [
+                    "url": components.string,
+                ],
+                kAFServiceOptionCallbackScheme: "logto-demo",
+            ]) { response in
+                guard let response = response, response.responseCode == AFResCode.success else {
+                    configuration
+                        .errorHandler(LogtoSocialPluginAlipayError
+                            .authFailed(withCode: response?.responseCode.rawValue.description ?? "unknown"))
+                    return
+                }
+
+                callbackComponents.queryItems = (callbackComponents.queryItems ?? []) + response.result
+                    .map { key, value in
+                        URLQueryItem(name: key.description, value: String(describing: value))
+                    }
+
+                guard let url = callbackComponents.url else {
+                    configuration.errorHandler(LogtoSocialPluginUriError.unableToConstructCallbackUri)
+                    return
+                }
+
+                configuration.completion(url)
+            }
+        }
+
+        public func handle(url: URL) -> Bool {
+            // https://opendocs.alipay.com/open/218/wy75xo#iOS%20%E7%A4%BA%E4%BE%8B
+            guard url.host == "apmqpdispatch" else {
+                return false
+            }
+
+            AFServiceCenter.handleResponseURL(url) { _ in
+                print("the app has been killed")
+            }
+
+            return true
         }
     }
 
-    public func handle(url: URL) -> Bool {
-        // https://opendocs.alipay.com/open/218/wy75xo#iOS%20%E7%A4%BA%E4%BE%8B
-        guard url.host == "apmqpdispatch" else {
-            return false
-        }
-
-        AFServiceCenter.handleResponseURL(url) { _ in
-            print("the app has been killed")
-        }
-
-        return true
-    }
-}
+#endif
